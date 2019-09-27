608c4406b7b5316b61cdab6b41f98d8032ee0a65
RaynoldTanYX <raynoldtanyx@gmail.com>
2019-09-27 12:41:52 +0800

    Skeleton and start of main menu

---
+++ CZ3003-SEHero/Assets/Scripts/Main Menu/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MainMenuController : MonoBehaviour
+{
+
+    private enum MenuState
+    {
+        Login,//0
+        Game,
+        World,
+        Level,
+        UserCreations,
+        Profile,
+        End
+    }
+
+	private MenuState m_state;
+
+    [SerializeField]
+    private List<GameObject> m_menuObjects;
+
+	void Start()
+	{
+		m_state = MenuState.Login;
+	}
+
+	public void ChangeState(int newState)
+	{
+        m_menuObjects[(int)m_state].SetActive(false);
+
+        m_state = (MenuState)newState;
+
+        m_menuObjects[(int)m_state].SetActive(true);
+    }
+}
