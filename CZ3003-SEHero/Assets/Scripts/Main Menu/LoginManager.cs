d2f86fd6e64013bfa442c5cebf075588aea58173
Hong Yong <hongyongwhy@users.noreply.github.com>
2019-10-09 00:22:27 +0800

    Added login and registration

---
+++ CZ3003-SEHero/Assets/Scripts/Main Menu/LoginManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour
+{
+
+    public InputField emailField;
+    public InputField passwordField;
+    public MainMenuController mc;
+    public Text welcomeBackText;
+
+    public void Login()
+    {
+        StartCoroutine(SendLogin(emailField.text, passwordField.text));
+    }
+
+    public void Register()
+    {
+        SceneManager.LoadScene("RegisterScene");
+    }
+
+    IEnumerator SendLogin(string email, string password)
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("email", email));
+        formData.Add(new MultipartFormDataSection("password", password));
+
+        Debug.Log("email " + email);
+        Debug.Log("password " + password);
+
+        UnityWebRequest www = UnityWebRequest.Post("http://3.1.70.5/login.php", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            var response = JSON.Parse(www.downloadHandler.text);
+            bool success = response["success"].AsBool;
+
+            if (success) {
+                GlobalVariables.username = response["username"];
+                mc.ChangeState(1);
+            }
+
+        }
+    }
+}
---
+++ CZ3003-SEHero/Assets/Scripts/Main Menu/RegistrationManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class RegistrationManager : MonoBehaviour
+{
+    public InputField usernameField;
+    public InputField passwordField;
+    public InputField emailField;
+    public GameObject messagePanel;
+    public Text message;
+    public MainMenuController mc;
+
+    public void Register()
+    {
+        StartCoroutine(SendRegistration(usernameField.text, passwordField.text, emailField.text));
+    }
+
+    IEnumerator ShowMessage(string msg)
+    {
+        messagePanel.SetActive(true);
+        message.text = msg;
+        yield return new WaitForSeconds(3f);
+        messagePanel.SetActive(false);
+
+        mc.ChangeState(0);
+    }
+
+    IEnumerator SendRegistration(string username, string password, string email)
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("username", username));
+        formData.Add(new MultipartFormDataSection("password", password));
+        formData.Add(new MultipartFormDataSection("email", email));
+
+        UnityWebRequest www = UnityWebRequest.Post("http://3.1.70.5/register.php", formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            var response = JSON.Parse(www.downloadHandler.text);
+            bool success = response["success"].AsBool;
+            Debug.Log(response);
+
+            if (success)
+            {
+                StartCoroutine(ShowMessage("Successfully registered!"));
+            }
+        }
+    }
+}
