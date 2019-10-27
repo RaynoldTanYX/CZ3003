8354ee61708bb8546684acae3757fd9f848cadfe
Hong Yong <hongyongwhy@users.noreply.github.com>
2019-10-27 20:39:02 +0800

    Added ability to change between 5 worlds

--- CZ3003-SEHero/Assets/Scripts/Main Menu/MainMenuController.cs
+++ CZ3003-SEHero/Assets/Scripts/Main Menu/MainMenuController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class MainMenuController : MonoBehaviour
@@ -25,6 +26,16 @@ public class MainMenuController : MonoBehaviour
     [SerializeField]
     private List<GameObject> m_menuObjects;
 
+    [SerializeField]
+    private Text worldTitleText;
+
+    [SerializeField]
+    private Text worldNumberText;
+
+    private int world = 1, numOfWorlds = 5;
+
+    private string[] worldDesc = {"Requirement Elicitation", "World 2 Desc", "World 3 Desc", "World 4 Desc", "World 5 Desc"};
+
 	void Start()
 	{
 		m_state = MenuState.Login;
@@ -39,10 +50,32 @@ public class MainMenuController : MonoBehaviour
         m_menuObjects[(int)m_state].SetActive(true);
     }
 
+    public void NextWorld() 
+    {
+        world++;
+
+        if (world > numOfWorlds)
+            world = 1;
+
+        worldTitleText.text = worldDesc[world - 1];
+        worldNumberText.text = "World # " + world;
+    }
+
+    public void PreviousWorld()
+    {
+        world--;
+
+        if (world < 1)
+            world = numOfWorlds;
+
+        worldTitleText.text = worldDesc[world - 1];
+        worldNumberText.text = "World # " + world;
+    }
+
     public void GetLevel(int level)
     {
         //TODO: add world selection
-        int world = 1;
+        //int world = 1;
 
         PlayerPrefs.SetInt("worldid", world);
         PlayerPrefs.SetInt("levelid", level);
