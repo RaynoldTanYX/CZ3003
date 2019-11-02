1a94b5c12e64c58bbc58fe160a18a8d952b86df5
Hong Yong <hongyongwhy@users.noreply.github.com>
2019-11-02 23:42:42 +0800

    Auto login user if PlayerPrefs is set. Logout() clears PlayerPrefs 'username 'key

--- CZ3003-SEHero/Assets/Scripts/Main Menu/LoginManager.cs
+++ CZ3003-SEHero/Assets/Scripts/Main Menu/LoginManager.cs
@@ -12,19 +12,50 @@ public class LoginManager : MonoBehaviour
     public InputField passwordField;
     public MainMenuController mc;
     public Text welcomeBackText;
+    public Text message;
+    public GameObject messagePanel;
 
     public DatabaseManager dbManager;
 
+    public void Start() 
+    {
+        if (PlayerPrefs.HasKey("username"))
+        {
+            welcomeBackText.text = "Welcome back, " + PlayerPrefs.GetString("username");
+            mc.ChangeState(1);
+        }
+    }
+
     public void Login()
     {
-        StartCoroutine(dbManager.SendLogin(emailField.text, passwordField.text, LoginCallback));
+        if (emailField.text.Length == 0 || passwordField.text.Length == 0)
+            StartCoroutine(ShowMessage("All fields must be filled up!"));
+        else
+            StartCoroutine(dbManager.SendLogin(emailField.text, passwordField.text, LoginCallback));
+    }
+
+    public void Logout() {
+        PlayerPrefs.DeleteKey("username");
+        mc.ChangeState(0);
+    }
+
+    IEnumerator ShowMessage(string msg)
+    {
+        messagePanel.SetActive(true);
+        message.text = msg;
+        yield return new WaitForSeconds(3f);
+        messagePanel.SetActive(false);
     }
 
     private void LoginCallback(bool success, string username) {
-        if(success) {
+        if (success)
+        {
             PlayerPrefs.SetString("username", username);
             mc.ChangeState(1);
             welcomeBackText.text = "Welcome back, " + username;
+        }
+        else {
+            StartCoroutine(ShowMessage("Login failed, please check your credentials."));
         }
     }
 }
